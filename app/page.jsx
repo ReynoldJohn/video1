fb6937a5797798f59021509ad631dce3788431af
ReynoldJohn <149870919+ReynoldJohn@users.noreply.github.com>
2024-01-17 07:40:06 +0000

    122

--- app/page.jsx
+++ app/page.jsx
@@ -66,58 +66,8 @@ export default function Home() {
         <VideoPlayer options={videoJsOptions} ima={ima} onReady={handlePlayerReady} />
       </div>
 
-      <div className="space-y-8 mt-10">
-        <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo tempore ad voluptate a aut consectetur, magnam
-          natus maxime quidem deserunt sed assumenda? Quo accusamus unde tempore ad eos sapiente, modi dolores earum
-          aliquid quia corrupti commodi soluta ipsum necessitatibus odit, perspiciatis cupiditate quisquam asperiores,
-          repellat excepturi deleniti facilis quod beatae! Ullam odit molestiae possimus facere illum, totam distinctio
-          dolores. Iure quidem iusto architecto iste quos doloremque mollitia optio ipsum accusamus? Nesciunt libero
-          quia, unde iusto hic doloremque blanditiis incidunt? Quidem, maiores esse. Ipsum velit odio ducimus natus et
-          impedit mollitia neque cupiditate, rem, rerum repudiandae molestias nesciunt aspernatur reprehenderit delectus
-          nisi beatae. Voluptate deleniti aut culpa sit saepe id placeat. Labore temporibus sint nulla eius et harum
-          veniam quo aliquam animi earum! Voluptatibus in ratione necessitatibus? Tempora nemo temporibus aliquam
-          numquam molestiae nam vero necessitatibus aspernatur omnis facilis expedita laborum qui itaque ipsam magnam
-          maxime atque, maiores enim fugiat, nulla architecto in quod! Debitis rerum quasi veniam commodi dolor soluta
-          cum quaerat porro asperiores eum, obcaecati eos voluptate sit similique molestiae repellat quis doloremque.
-          Tenetur tempora quos cumque ab recusandae facilis molestiae. Officia expedita itaque soluta atque hic animi
-          quas eius, ipsam quam, at exercitationem cumque ea possimus. Quae, in.
-        </p>
-
-        <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo tempore ad voluptate a aut consectetur, magnam
-          natus maxime quidem deserunt sed assumenda? Quo accusamus unde tempore ad eos sapiente, modi dolores earum
-          aliquid quia corrupti commodi soluta ipsum necessitatibus odit, perspiciatis cupiditate quisquam asperiores,
-          repellat excepturi deleniti facilis quod beatae! Ullam odit molestiae possimus facere illum, totam distinctio
-          dolores. Iure quidem iusto architecto iste quos doloremque mollitia optio ipsum accusamus? Nesciunt libero
-          quia, unde iusto hic doloremque blanditiis incidunt? Quidem, maiores esse. Ipsum velit odio ducimus natus et
-          impedit mollitia neque cupiditate, rem, rerum repudiandae molestias nesciunt aspernatur reprehenderit delectus
-          nisi beatae. Voluptate deleniti aut culpa sit saepe id placeat. Labore temporibus sint nulla eius et harum
-          veniam quo aliquam animi earum! Voluptatibus in ratione necessitatibus? Tempora nemo temporibus aliquam
-          numquam molestiae nam vero necessitatibus aspernatur omnis facilis expedita laborum qui itaque ipsam magnam
-          maxime atque, maiores enim fugiat, nulla architecto in quod! Debitis rerum quasi veniam commodi dolor soluta
-          cum quaerat porro asperiores eum, obcaecati eos voluptate sit similique molestiae repellat quis doloremque.
-          Tenetur tempora quos cumque ab recusandae facilis molestiae. Officia expedita itaque soluta atque hic animi
-          quas eius, ipsam quam, at exercitationem cumque ea possimus. Quae, in.
-        </p>
-
-        <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo tempore ad voluptate a aut consectetur, magnam
-          natus maxime quidem deserunt sed assumenda? Quo accusamus unde tempore ad eos sapiente, modi dolores earum
-          aliquid quia corrupti commodi soluta ipsum necessitatibus odit, perspiciatis cupiditate quisquam asperiores,
-          repellat excepturi deleniti facilis quod beatae! Ullam odit molestiae possimus facere illum, totam distinctio
-          dolores. Iure quidem iusto architecto iste quos doloremque mollitia optio ipsum accusamus? Nesciunt libero
-          quia, unde iusto hic doloremque blanditiis incidunt? Quidem, maiores esse. Ipsum velit odio ducimus natus et
-          impedit mollitia neque cupiditate, rem, rerum repudiandae molestias nesciunt aspernatur reprehenderit delectus
-          nisi beatae. Voluptate deleniti aut culpa sit saepe id placeat. Labore temporibus sint nulla eius et harum
-          veniam quo aliquam animi earum! Voluptatibus in ratione necessitatibus? Tempora nemo temporibus aliquam
-          numquam molestiae nam vero necessitatibus aspernatur omnis facilis expedita laborum qui itaque ipsam magnam
-          maxime atque, maiores enim fugiat, nulla architecto in quod! Debitis rerum quasi veniam commodi dolor soluta
-          cum quaerat porro asperiores eum, obcaecati eos voluptate sit similique molestiae repellat quis doloremque.
-          Tenetur tempora quos cumque ab recusandae facilis molestiae. Officia expedita itaque soluta atque hic animi
-          quas eius, ipsam quam, at exercitationem cumque ea possimus. Quae, in.
-        </p>
+      
       </div>
-    </div>
+    
   );
 }
