ecbae027b14070a85b7a9e8a5f0347b8ca62f5d7
ReynoldJohn <149870919+ReynoldJohn@users.noreply.github.com>
2024-01-17 07:52:25 +0000

    done

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,10 @@
 import { useRef } from 'react';
 import VideoPlayer from '../components/VideoPlayer';
 
-const ima = {
-  adTagUrl:
-    'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
-    'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
-    'impl=s&gdfp_req=1&env=vp&output=xml_vmap1&unviewed_position_start=1&' +
-    'cust_params=sample_ar%3Dpremidpostpod%26deployment%3Dgmf-js' +
-    '&cmsid=496&vid=short_onecue&correlator=',
-};
 
 const videoJsOptions = {
   autoplay: true,
-  muted: true,
+  muted: false,
   controls: true,
   responsive: true,
   fluid: true,
@@ -34,24 +26,7 @@ export default function Home() {
   const handlePlayerReady = (player) => {
     playerRef.current = player;
 
-    player.on('adsready', function () {
-      var events = [
-        google.ima.AdEvent.Type.ALL_ADS_COMPLETED,
-        google.ima.AdEvent.Type.CLICK,
-        google.ima.AdEvent.Type.COMPLETE,
-        google.ima.AdEvent.Type.FIRST_QUARTILE,
-        google.ima.AdEvent.Type.LOADED,
-        google.ima.AdEvent.Type.MIDPOINT,
-        google.ima.AdEvent.Type.PAUSED,
-        google.ima.AdEvent.Type.RESUMED,
-        google.ima.AdEvent.Type.STARTED,
-        google.ima.AdEvent.Type.THIRD_QUARTILE,
-      ];
-
-      for (var index = 0; index < events.length; index++) {
-        player.ima.addEventListener(events[index], onAdEvent.bind(this));
-      }
-    });
+    
 
     function onAdEvent(event) {
       console.log(event);
@@ -63,7 +38,7 @@ export default function Home() {
       <h1 className="text-5xl font-semibold text-center text-neutral-800">Video.js IMA Example</h1>
 
       <div className="mt-10">
-        <VideoPlayer options={videoJsOptions} ima={ima} onReady={handlePlayerReady} />
+        <VideoPlayer options={videoJsOptions}  onReady={handlePlayerReady} />
       </div>
 
       
--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -1,8 +1,7 @@
 'use client';
 import React, { useEffect, useRef, useState } from 'react';
 import videojs from 'video.js';
-import 'videojs-contrib-ads';
-import 'videojs-ima';
+
 
 import 'video.js/dist/video-js.css';
 import '@/app/video-skin.css';
@@ -14,15 +13,12 @@ const VideoPlayer = (props) => {
   const videoWrapperRef = useRef(null);
   const { options, onReady, ima } = props;
 
-  const [showCloseBtn, setShowCloseBtn] = useState(false);
-  const [showSkipBtn, setShowSkipBtn] = useState(false);
+  
 
   const [showVideo, setShowVideo] = useState(true);
 
   React.useEffect(() => {
-    // Make sure Video.js player is only initialized once
     if (!playerRef.current) {
-      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
       const videoElement = document.createElement('video-js');
 
       videoElement.classList.add('vjs-big-play-centered');
@@ -35,8 +31,7 @@ const VideoPlayer = (props) => {
         player.ima(ima);
       }));
 
-      // You could update an existing player in the `else` block here
-      // on prop change, for example:
+     
     } else {
       const player = playerRef.current;
 
@@ -45,7 +40,6 @@ const VideoPlayer = (props) => {
     }
   }, [options, videoRef]);
 
-  // Dispose the Video.js player when the functional component unmounts
   useEffect(() => {
     const player = playerRef.current;
 
@@ -58,30 +52,25 @@ const VideoPlayer = (props) => {
   }, [playerRef]);
 
   useEffect(() => {
-    // Get the video player's height and set it to the video wrapper
     videoWrapperRef.current.style.height = videoRef.current.clientHeight + 'px';
 
     const observer = new IntersectionObserver(
       (entries) => {
-        // The callback is called whenever the visibility of the target changes
         entries.forEach((entry) => {
           setIsFixed(!entry.isIntersecting);
         });
       },
       {
-        // You can customize the root, rootMargin, and threshold options
-        root: null, // Use the viewport as the root
-        rootMargin: '0px', // No margin
-        threshold: 0.2, // Trigger when 50% of the target is visible
+        root: null, 
+        rootMargin: '0px', 
+        threshold: 0.2,
       }
     );
 
-    // Start observing the target element
     if (videoWrapperRef.current) {
       observer.observe(videoWrapperRef.current);
     }
 
-    // Cleanup observer on component unmount
     return () => {
       if (videoWrapperRef.current) {
         observer.unobserve(videoWrapperRef.current);
@@ -110,28 +99,7 @@ const VideoPlayer = (props) => {
   return (
     <div ref={videoWrapperRef} className="relative">
       <div className="absolute z-[9999] top-1 right-1 flex gap-1">
-        {showSkipBtn && (
-          <button
-            className="bg-[#00b2ff] text-white text-xs font-medium py-1 px-2"
-            onClick={() => {
-              setShowSkipBtn((prev) => !prev);
-              setShowVideo(false);
-            }}
-          >
-            Skip
-          </button>
-        )}
-        {showCloseBtn && (
-          <button
-            className="bg-[#00b2ff] text-white text-xs font-medium py-0.5 px-3"
-            onClick={() => {
-              setShowCloseBtn((prev) => !prev);
-              setShowVideo(false);
-            }}
-          >
-            Close
-          </button>
-        )}
+      
       </div>
 
       <div className={isFixed ? 'fixed bottom-4 right-4 h-56 aspect-video' : ''}>
